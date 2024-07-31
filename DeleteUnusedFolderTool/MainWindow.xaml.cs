f6f9a54d1efdc149f0441ee683b3381c9962d7f1
NkcNakano0203 <95272840+NkcNakano0203@users.noreply.github.com>
2024-08-01 00:28:59 +0900

    フォルダ構成を整理

--- DeleteUnusedFolderTool/MainWindow.xaml.cs
+++ DeleteUnusedFolderTool/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using Microsoft.WindowsAPICodePack.Dialogs;
